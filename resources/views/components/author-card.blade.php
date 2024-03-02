b81b1f1614aaa5409ed26c3e1979435f1c9406c5
NIK0LADZE <giorgi.nikoladze@scandiweb.com>
2024-03-03 01:06:07 +0400

    fix author-card component bug

--- resources/views/components/author-card.blade.php
+++ resources/views/components/author-card.blade.php
@@ -1,7 +1,7 @@
-@props(['author', 'authorInfoClass'])
+@props(['author', 'isPostPage' => false])
 <div {{ $attributes->merge(['class' => 'flex items-center text-sm']) }}>
     <img src="/images/lary-avatar.svg" alt="Lary avatar">
-    <div class="ml-3 {{ $authorInfoClass }}">
+    <div class="ml-3{{ $isPostPage ? ' text-left': '' }}">
         <h5 class="font-bold">{{ $author->name }}</h5>
         <h6>Mascot at Laracasts</h6>
     </div>
