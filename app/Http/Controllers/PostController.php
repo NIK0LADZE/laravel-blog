57717e4d95f76055fad0aa8c232d5dc7c382244e
NIK0LADZE <giorgi.nikoladze@scandiweb.com>
2024-03-03 21:02:03 +0400

    Implement pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,8 +10,9 @@ class PostController extends Controller
 {
     public function index()
     {
+        // return Post::latest('published_at')->filter(request(['search']))->get();
         return view('posts.showMany', [
-            'posts' => Post::latest('published_at')->filter(request(['search']))->get(),
+            'posts' => Post::latest('published_at')->filter(request(['search']))->paginate(6)->withQueryString(),
         ]);
     }
 
@@ -26,14 +27,14 @@ class PostController extends Controller
     public function showCategoryPosts(Category $category)
     {
         return view('posts.showMany', [
-            'posts' => $category->posts()->latest('published_at')->filter(request(['search']))->get(),
+            'posts' => $category->posts()->latest('published_at')->filter(request(['search']))->paginate(6)->withQueryString(),
         ]);
     }
 
     public function showAuthorPosts(User $author)
     {
         return view('posts.showMany', [
-            'posts' => $author->posts()->latest('published_at')->filter(request(['search']))->get(),
+            'posts' => $author->posts()->latest('published_at')->filter(request(['search']))->paginate(6)->withQueryString(),
         ]);
     }
 }
