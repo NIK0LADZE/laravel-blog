6436609b7952f224b6c23d2410eab5cd0ce7a238
NIK0LADZE <giorgi.nikoladze@scandiweb.com>
2024-03-03 18:27:14 +0400

    adjust DB seeder and category factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,18 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = User::factory()->create([
-            'name' => 'Jane Doe'
-        ]);
+        // $user = User::where('username', 'jane.doe')->first() ?? User::factory()->create([
+        //     'username' => 'jane.doe',
+        //     'name' => 'Jane Doe'
+        // ]);
+        $users = User::factory(10)->create();
 
-        $category = Category::factory()->create([
-            'name' => 'Movies',
-            'slug' => 'movies'
-        ]);
+        // $category = Category::where('name', 'Anime')->first() ?? Category::factory()->create([
+        //     'name' => 'Anime',
+        //     'slug' => 'anime'
+        // ]);
+        $categories = Category::factory(18)->create();
 
-        Post::factory(5)->create([
-            'user_id' => $user->id,
-            'category_id' => $category->id
+        Post::factory(100)->create([
+            'user_id' => function () use ($users) {
+                return $users->random()->id;
+            },
+            'category_id' => function () use ($categories) {
+                return $categories->random()->id;
+            }
         ]);
     }
 }
